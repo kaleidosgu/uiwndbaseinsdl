38063cf660bc539bbe9460dcbc9b5d0df0a3f8db
kaleido <tsaoyi@gmail.com>
2010-08-10 14:36:44 +0800

    add onupdate , ondraw and listchild function.

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -17,6 +17,11 @@ void CDraw::OnDraw()
 {
 	if( m_bShow )
 	{
+		for( std::list < CDraw* >::iterator it = m_lstChild.begin(); it != m_lstChild.end(); ++it )
+		{
+			CDraw* pDraw = (*it);
+			pDraw->OnDraw();
+		}
 	}
 }
 void CDraw::SetRect( const SDL_Rect& rect )
@@ -50,4 +55,15 @@ void CDraw::Create( Sint16 x, Sint16 y, Sint16 w, Sint16 h )
 	m_rect.y = y;
 	m_rect.w = w;
 	m_rect.h = h;
+}
+void CDraw::OnUpdate()
+{
+	if( m_bShow )
+	{
+		for( std::list < CDraw* >::iterator it = m_lstChild.begin(); it != m_lstChild.end(); ++it )
+		{
+			CDraw* pDraw = (*it);
+			pDraw->OnUpdate();
+		}
+	}
 }
--- src/Draw.h
+++ src/Draw.h
@@ -2,6 +2,7 @@
 #include "SDL_video.h"
 #include "SDL_stdinc.h"
 #include "DllExport.h"
+#include <list>
 class WND_EXPORT CDraw
 {
 public:
@@ -9,6 +10,7 @@ public:
 	virtual ~CDraw(void);
 	virtual void Create( Sint16 x, Sint16 y, Sint16 w, Sint16 h );
 	virtual void OnDraw();
+	virtual void OnUpdate();
 	virtual void SetRect( const SDL_Rect& rect );
 	virtual void SetRectAttr( const Sint16& x, const Sint16& y, const Uint16& w, const Uint16& h );
 	virtual void SetPos( Sint16 x, Sint16 y );
@@ -16,6 +18,7 @@ public:
 	bool IsVisible() const;
 private:
 	bool m_bShow;
+	std::list < CDraw* > m_lstChild;
 protected:
 	SDL_Rect m_rect;
 };
